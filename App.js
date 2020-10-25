7e1d4c7e4362117adfdbd8adda20e3ceafcb8b0c
lucthanhlam <65240304+lucthanhlam@users.noreply.github.com>
2020-10-26 06:16:17 +0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,143 @@
+import React from 'react';
+import { StyleSheet, Text, View ,TouchableOpacity , FlatList, Modal , Clipboard, ActivityIndicator} from 'react-native';
+import {AntDesign} from '@expo/vector-icons';
+import colors from './Colors';
+import tempData from './tempData';
+import TodoList from './components/TodoList';
+import AddListModal from './components/AddListModal';
+import Fire from './Fire';
+
+
+export default class App extends React.Component {
+  state={
+    addTodoVisible:false,
+    lists:[],
+    user: {},
+    loading:true
+  };
+
+  componentDidMount(){
+    firebase = new Fire((error , user) => {
+      if(error){
+        return alert("Da co loi xay ra!");
+      }
+
+      firebase.getLists(list => {
+        this.setState({lists,user},() => {
+          this.setState({loading:false});
+        });
+      });
+
+      this.setState({user});
+    });
+  }
+
+  componentWillUnmount(){
+    firebase.detach();
+  }
+
+  toggleAddTodoModal(){
+    this.setState({addTodoVisible: !this.state.addTodoVisible})
+  };
+
+  renderList = list => {
+    return <TodoList list={list} updateList={this.updateList} />
+  };
+
+  addList = list => {
+    firebase.addList({
+      name:list.name,
+      color:list.color,
+      todos:[]
+    });
+  };
+
+  updateList = list =>{
+    firebase.updateList(list);
+  };
+
+  render(){
+    if(this.state.loading){
+      return(
+        <View style={styles.container}>
+            <ActivityIndicator size="large" color={colors.blue}/>
+        </View>
+      );
+    };
+    return (
+      <View style={styles.container}>
+
+        <Modal 
+          animationType="slide" 
+          visible={this.state.addTodoVisible}
+          onRequestClose={() => this.toggleAddTodoModal()}
+        >
+          <AddListModal closeModal={() => this.toggleAddTodoModal()} addList={this.addList}/>
+        </Modal>
+        
+        <View style={{flexDirection:"row"}}>
+            <View style={styles.diviner}/>
+            <Text style={styles.title}>
+              ToDo <Text style={{fontWeight:"300",color:colors.blue}}>List!</Text>
+            </Text>
+            <View style={styles.diviner}/>
+        </View>
+
+        <View style={{marginVertical:48}}>
+          <TouchableOpacity style={styles.addList} onPress={() => this.toggleAddTodoModal()}>
+            <AntDesign name="plus" size={16} color={colors.blue}/>
+          </TouchableOpacity>
+
+          <Text style={styles.add}>Add List</Text>
+        </View>
+
+        <View style={{height:275,paddingLeft:32}}>
+          <FlatList 
+            data={this.state.lists} 
+            keyExtractor={item => item.id.toString()} 
+            horizontal={true} 
+            showsHorizontalScrollIndicator={false}
+            renderItem={({ item }) => this.renderList(item) }
+            keyboardShouldPersistTaps="always"
+          />
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  //<Text onPress={() => Clipboard.setString(this.state.user.uid)}>User:{this.state.user.uid}</Text>
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  diviner:{
+    backgroundColor:colors.lighBlue,
+    height:1,
+    flex:1,
+    alignSelf:"center",
+  },
+  title:{
+    fontSize:38,
+    fontWeight:"800",
+    color:colors.black,
+    paddingHorizontal:64,
+  },
+  addList:{
+    borderWidth:2,
+    borderColor:colors.lighBlue,
+    borderRadius:4,
+    padding: 16,
+    alignItems:"center",
+    justifyContent:"center",
+  },
+  add:{
+    color:colors.blue,
+    fontWeight:"600",
+    fontSize:14,
+    marginTop:8,
+  }
+});
